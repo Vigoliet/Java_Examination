67f00e9316fd4bb77cb09a283a442a09dc3b161b
Vigor Lundkvist <vigor.lundkvist@live.se>
2023-10-20 22:01:46 +0200

    Added User attributes and created methodnames for needed methods in UserList

---
+++ src/main/java/org/example/UserList.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class UserList {
+    private ArrayList<User> users = new ArrayList<>();
+
+    // Method for adding User to the arrayList
+    public void addUser(User userToAdd){
+
+    }
+
+    // Method for deleting User by id
+    public void removeUser(int id){
+
+    }
+
+    // Method for printing the arrayList of users
+    public void printUserList(){
+
+    }
+
+    // Method for sorting arrayList of objects by alphabetical order
+    public void sortUsersByName(){
+
+    }
+}

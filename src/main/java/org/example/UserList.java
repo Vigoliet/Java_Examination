f4b6a1a7773a4d9314e8de65c2b020eea95768de
Vigor Lundkvist <vigor.lundkvist@live.se>
2023-10-20 22:11:31 +0200

    Implemented methods

--- src/main/java/org/example/UserList.java
+++ src/main/java/org/example/UserList.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class UserList {
     private ArrayList<User> users = new ArrayList<>();
@@ -12,7 +13,7 @@ public class UserList {
 
     // Method for deleting User by id
     public void removeUser(int id){
-
+        users.removeIf(user -> user.getId() == id); // Lambda expression for searching for given ID
     }
 
     // Method for printing the arrayList of users
@@ -20,13 +21,18 @@ public class UserList {
         System.out.println(users);
     }
 
-    // Method for sorting arrayList of objects by alphabetical order
+    // Method for sorting array list of objects by alphabetical order with a comparator
     public void sortUsersByName(){
-
+        users.sort(Comparator.comparing(User::getName));
     }
 
     // Method for searching after index in list
-    public User searchUserById(int id){
-        return null;
+    public User searchUserById(int id) {
+        for (User user : users) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+        return null; // Return null if the user is not found
     }
 }
